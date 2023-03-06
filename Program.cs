299e4b84321736640b1b28662eee15d79bc0a3dd
Maksim <dvmax1984@mail.ru>
2023-03-06 15:02:50 +0700

    Решили задачу 28 методами и классом

--- Program.cs
+++ Program.cs
@@ -1,22 +1,6 @@
-﻿Console.WriteLine("Задача 28: Напишите программу, которая принимает на вход число N и выдаёт произведение чисел от 1 до N.");
+﻿using static Library;
 
-static int GetNumber(string s )
-{
-    Console.Write(s);
-    return Convert.ToInt32(Console.ReadLine());
-}
-
-static void GetResult(int n)
-{
-    int p = 1;
-
-    for (int i = 1; i < n; i++)
-    {
-        p = p * (i + 1);
-
-        Console.WriteLine(p);
-    }
-}
+Console.WriteLine("Задача 28: Напишите программу, которая принимает на вход число N и выдаёт произведение чисел от 1 до N.");
 
 int n = GetNumber("Введите число N: ");
 GetResult(n);
