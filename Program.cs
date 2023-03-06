3a1a409c85f6dfac96ee4b33fa68687c427c4e52
Maksim <dvmax1984@mail.ru>
2023-03-06 14:52:52 +0700

    Решили задачу 28

--- Program.cs
+++ Program.cs
@@ -1,2 +1,14 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("Задача 28: Напишите программу, которая принимает на вход число N и выдаёт произведение чисел от 1 до N.");
+
+
+Console.Write("Введите число N: ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+int p = 1;
+
+for (int i = 1; i < n; i++)
+{
+    p = p * (i + 1);
+       
+    Console.WriteLine(p);
+}
